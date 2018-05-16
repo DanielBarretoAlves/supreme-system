7932f7c287f726a3f2bd93e59a975760e46cad66
Daniel Barreto <danielbarreto559@gmail.com>
2018-05-16 01:24:05 -0300

    Add files via upload

--- NEO/loginValidation.js
+++ NEO/loginValidation.js
@@ -1,8 +1,23 @@
-function myFunction() {
+function showPassword() {
     var x = document.getElementById("password");
     if (x.type === "password") {
         x.type = "text";
     } else {
         x.type = "password";
     }
+}
+
+function login(){
+  var name, text;
+  var password, password;
+
+  //get the name
+  name = document.getElementById("name").value;
+  password = document.getElementById("password").value;
+  if(password == "password" && name == "admin"){
+    alert("Ta funfando");
+    window.location.href="#";
+  }else{
+    alert("Error wrong Password or User Name");
+  }
 }
